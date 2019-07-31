742baef9fee38c8966399c864c75ce8d338f1fa9
otodaceh <45671712+otodaceh@users.noreply.github.com>
2019-07-31 14:07:13 -0400

    Create configFromFile.py

---
+++ configFromFile.py
@@ -0,0 +1,23 @@
+!/usr/bin/env python
+
+from netmiko import ConnectHandler
+from getpass import getpass
+
+cisco_1921 = {
+    'device_type': 'cisco_ios',
+    'host': raw_input("What is the router's IP? "),
+    'username': raw_input("What is your username? "),
+    'password': getpass(),
+    'port' : 22,          # optional, defaults to 22
+    'secret': 'secret',     # optional, defaults to ''
+}
+
+with open('config') as f:
+    lines = f.read().splitlines()
+#print lines #Optional, prints each line to be pushed to router
+net_connect = ConnectHandler(**cisco_1921)
+output = net_connect.send_config_set(lines)
+print output 
+
+net_connect.disconnect()
+Print ("Status: Successful!")
