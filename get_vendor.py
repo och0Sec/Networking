56c7a9b7d91d3bec0f75dacdef712ca3cdfabe27
otodaceh <45671712+otodaceh@users.noreply.github.com>
2019-11-15 10:04:24 -0500

    Update get_vendor.py

--- get_vendor.py
+++ get_vendor.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+
+# Requires maclookup to be installed.
+# to do that, run: sudo pip3 install maclookup
+# After that sign up at https://macaddress.io/ to get your free API key.
+
 from maclookup import ApiClient
 
 def vendor(oui):
-    client = ApiClient('Your API key') #from https://macaddress.io/
+    client = ApiClient('Your API key') 
     vend = client.get_vendor(oui)
     vend = str(vend.decode('utf-8'))
     return vend
